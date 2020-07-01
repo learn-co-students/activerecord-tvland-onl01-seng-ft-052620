cc2b92f8313bf7a6e4948dd5e8f2b8028caf4315
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-07-01 20:21:03 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,22 @@
+require 'pry'
+
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    full = "#{self.first_name} #{self.last_name}"
+    full
+  end
+
+  def list_roles
+    roles = []
+      self.characters.each do |character|
+      binding.pry
+        roles << "#{character.name} - #{character.show}"
+      end
+    roles
+
+  end
+
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,14 @@
 class Show < ActiveRecord::Base
-  
+
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :networks
+
+  def actors_list
+    list = []
+    self.actors.each do |actor|
+      list << actor.full_name
+    end
+  end
+
 end
