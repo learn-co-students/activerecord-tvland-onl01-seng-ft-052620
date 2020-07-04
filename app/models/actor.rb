fcd317c6b8c4bb8630f452b3ceb6ffcc53fd611e
Donna Torres <todonnatorres@icloud.com>
2020-07-04 05:03:28 -0500

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,33 @@
 class Actor < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :shows, through: :characters
+
+    def full_name
+        "#{self.first_name} #{self.last_name}"
+    end
+
+    def list_roles
+        self.characters.collect do |character|
+            "#{character.name} - #{character.show.name}"
+        end
+    end
 end
+
+# - Write a method in the `Actor` class, `#list_roles`, that lists all of the
+#   characters that actor has alongside the show that the character is in. So, for
+#   instance, if we had an actor, Peter Dinklage, a character, Tyrion Lannister, and a show, Game of Thrones, we with
+
+#     ```ruby
+#     peter = Actor.new(:first_name => "Peter", :last_name => "Dinklage")
+#     tyrion = Character.new(:name => "Tyrion Lannister")
+#     tyrion.actor = peter
+#     thrones = Show.new(:name => "Game of Thrones")
+#     tyrion.show = thrones
+#     tyrion.save
+#     ```
+
+#   And then, when we run `peter.list_roles`, we would get an Array containing a
+#   string with both the character and the show:
+
+#     ```ruby
+#     ['Tyrion Lannister - Game of Thrones']
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,14 @@
 class Show < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :actors, through: :characters
+    belongs_to :network
+
+        def actors_list
+         self.characters.collect do |character|
+            character.actor.full_name
+        end
+    end
 end
+
+# Array of the full names of each actor associated with the a show. Remember,
+#   a show should have many actors through characters.
