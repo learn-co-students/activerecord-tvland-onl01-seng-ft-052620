56c3013c9751772b6acc4e32f9a09f5b412eefaa
vincentservio <vincentservio@gmail.com>
2020-08-23 22:32:06 -0400

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,16 @@
+
+
 class Show < ActiveRecord::Base
-  
+ has_many :characters
+ has_many :actors, through: :characters
+ belongs_to :network
+
+    def actors_list 
+        list = []
+        self.actors.collect do |x|
+           #binding.pry
+
+           x.first_name + " " + x.last_name
+        end 
+    end 
 end
